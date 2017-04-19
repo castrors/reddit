f17fec092adbe691ebe6f64831e732b600ebe426
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2017-04-19 15:17:36 -0300

    setup comment request result data

--- app/src/main/java/com/castrodev/reddit/api/ApiService.java
+++ app/src/main/java/com/castrodev/reddit/api/ApiService.java
@@ -2,8 +2,11 @@ package com.castrodev.reddit.api;
 
 import com.castrodev.reddit.model.RedditObject;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -15,5 +18,9 @@ public interface ApiService {
     @GET("/r/Android/new/.json")
     Call<RedditObject> fetchPosts(@Query("after") String after,
                                   @Query("limit") String limit);
+
+
+    @GET("{permalink}.json")
+    Call<List<RedditObject>> fetchComments(@Path(value = "permalink", encoded = true) String permalink);
 
 }
